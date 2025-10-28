a044342cff1b92dd095c1f9a81dd4e55b05046f1
Leonardo Sabbadin <leosabbadinads01@gmail.com>
2025-10-28 16:42:44 +0000

    as imagens ao subir na vercel não estão aparecendo, oq pode fazer?

--- next.config.ts
+++ next.config.ts
@@ -23,10 +23,10 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https' ,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
-pathname: '/**',
+        pathname: '/**',
       },
       {
         protocol: 'https',
