434be472e84317d706b8ee79d1329914e633b143
Leonardo Sabbadin <leosabbadinads01@gmail.com>
2025-10-28 16:34:26 +0000

    a capa e a logo do rodapé e do cabeçalho também não estão aparecendo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import type { SVGProps } from 'react';
 export default function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <Image
-      src="https://raw.githubusercontent.com/leosabbadin/imagem-abba/593d08e00bbd17757eb8206804b9a41a6afa60e9/Screenshot%20(31)%20(1).png"
+      src="https://raw.githubusercontent.com/leosabbadin/imagem-abba/main/Screenshot%20(31)%20(1).png"
       alt="Abba Physio Logo"
       width={100}
       height={50}
